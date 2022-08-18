3f674e03211215805e362c0ac0f25adc4598843a
aniket2910 <solankianiket0411@gmail.com>
2022-08-18 15:33:28 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const PORT = 8080;
+const path = require("path");
+const jsonServer = require("json-server");
+const bodyParser = require("body-parser");
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+server.use(middlewares);
+server.use(jsonServer, bodyParser);
+server.use("/api", router);
+server.listen(PORT, () => {
+  console.log("Server is running on 8080");
+});
